b449b612ebcd1753e1b9bd4574c6ea50e405cc30
Huang MingLei <huangminglei@Huangs-MBP.lan>
2018-05-21 23:48:17 +0800

    lesson 0

---
+++ src/com/wikid/lesson0/Main.java
@@ -0,0 +1,130 @@
+package com.wikid.lesson0;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Stack;
+import java.util.StringTokenizer;
+/*
+	问题描述
+	给定一个序列 A，请你将它升序排序。
+
+	输入格式
+	第一行一个正整数 n，表示序列长度。
+
+	第二行 n 个用空格隔开的非负整数，描述这个序列。
+
+	输出格式
+	n 行，每行一个非负整数，表示排序后的序列。
+
+	样例输入
+	4
+	1 3 2 10
+	样例输出
+	1
+	2
+	3
+	10
+	数据范围
+	保证 n<=1000，保证序列中的数不超过 32767
+*/
+public class Main {
+
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        Task solver = new Task();
+        solver.solve(in, out);
+        out.close();
+    }
+
+
+    static class Task {
+        
+        // myStack：输入栈，栈中的所有元素即是待排序的元素
+        // 返回值：输出栈，即排序后的序列，满足从栈底至栈顶为升序
+        Stack<Integer> sorting(Stack<Integer> myStack) {
+            Stack<Integer> result = new Stack<>(); // result存放了返回值，即输出栈
+            if (myStack.empty()) // 边界情况
+                return result;
+
+            int tmp = myStack.pop(); // tmp记录的是下一个要插入到result栈中的数
+            while (!myStack.empty() || (!result.empty() && result.peek() > tmp)) { // 注意后面的判断条件不要漏
+                // 如果将tmp压入到result是顺序的
+                // 那么可以直接将tmp压入进result栈中
+                // 并且从myStack栈中取出栈顶作为新的tmp
+                if (result.empty() || result.peek() <= tmp) {
+                    result.push(tmp);
+                    tmp = myStack.pop();
+                } else { // 否则我们得将比tmp大的元素（目前是result栈顶）重新放回myStack栈中
+                    myStack.push(result.pop());
+                }
+            }
+            result.push(tmp); // 最后别忘了将tmp压回result栈中
+            return result;
+        }
+
+        void solve(InputReader in, PrintWriter out) {
+            int n = in.nextInt();
+            Stack<Integer> myStack = new Stack<>();
+            while (!myStack.empty())
+                myStack.pop();
+            for (int i = 0; i < n; ++i) {
+                int tmp = in.nextInt();
+                myStack.push(tmp);
+            }
+
+            Stack<Integer> result = sorting(myStack);
+            for (Integer x : result)
+                out.println(x);
+        }
+
+    }
+
+    static class InputReader {
+        BufferedReader reader;
+        StringTokenizer tokenizer;
+
+        private InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        private String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        private short nextShort() {
+            return Short.parseShort(next());
+        }
+
+        private int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        private long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        private float nextFloat() {
+            return Float.parseFloat(next());
+        }
+
+        private double nextDouble() {
+            return Double.parseDouble(next());
+        }
+    }
+
+}
